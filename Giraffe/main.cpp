e9c602ddf7374dfef3cbcf32ab16c020fe9b15f3
CryptoBoomer321 <106280184+CryptoBoomer321@users.noreply.github.com>
2022-06-03 10:37:01 +0100

    New Files (C++ Project)
    C++ Test Project

---
+++ Giraffe/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*  This is a multi-line comment
+        You can use this for multiple lines of text
+        to describe a function etc...  */
+
+    // This is a single-line comment for more brief descriptions...
+
+
+    /*Character for a singular character*/
+    char grade = 'A';
+
+    /*String for multiple characters*/
+    string phrase = "Giraffe Academy";
+
+    /*Int for integers*/
+    int age = 50;
+
+    /*Float for decimals*/
+    float pda = 5.5;
+
+    /*Double for MORE decimals*/
+    double gpa = 2.3456;
+
+    /*Boolean for TRUE or FALSE*/
+    bool isMale = true;
+
+    /*Print a string, can be a constant OR a variable*/
+    //cout << grade << endl;
+
+    //cout << "Testing new lines \nTesting new line 2" << endl;
+
+    /*Print a strings length*/
+    //cout << phrase.length();
+
+    /*Print a specific character in a string -> STARTS AT 0 NOT 1*/
+    //cout << phrase[2];
+
+    /*Assign a new character in a string*/
+    //phrase[0] = 'B';
+    //cout << phrase;
+
+    return 0;
+}
